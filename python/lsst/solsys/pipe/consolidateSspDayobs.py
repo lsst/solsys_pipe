1a1e3c512682db345bc59e15fe6f61194105f47b
gerenjie <jkurla@uw.edu>
2025-07-30 12:02:48 -0700

    Break consolidateSsp into per-dayobs and pan-dayobs sections

--- python/lsst/solsys/pipe/consolidateSspDayobs.py
+++ python/lsst/solsys/pipe/consolidateSspDayobs.py
@@ -20,14 +20,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-"""A per-dayobs task which takes `goodSeeingDiff_assocDiaSrc` or
-`goodSeeingDiff_diaSrcTable` from the last 14 (config-settable) days and
-consolidates them into a `sspVisitInputs` as defined in `makeTracklets.py`
+"""A per-dayobs task which consolidates `goodSeeingDiff_assocDiaSrc`,
+`goodSeeingDiff_diaSrcTable` and consolidates them per-dayobs.`
 """
 
 __all__ = [
-    "ConsolidateSspTablesTask",
-    "ConsolidateSspTablesConfig",
+    "ConsolidateSspDayobsTask",
+    "ConsolidateSspDayobsConfig",
 ]
 
 
@@ -36,6 +35,7 @@ import logging
 import lsst.pex.config
 import lsst.pipe.base as pipeBase
 import numpy as np
+from . import utils
 from astropy import units as u
 from astropy.table import Table
 from heliolinx import solarsyst_dyn_geo as solardg
@@ -47,14 +47,14 @@ from lsst.solsys.pipe.utils import df2numpy
 _LOG = logging.getLogger(__name__)
 
 
-class ConsolidateSspTablesConnections(
+class ConsolidateSspDayobsConnections(
     pipeBase.PipelineTaskConnections,
     defaultTemplates={
-        "diaSourceInputName": "dia_source_detector",
+        "diaSourceInputName": "dia_source_visit",
         "earthStateInputName": "sspEarthState",
-        "diaSourceOutputName": "dia_source_dayobs_14",
+        "diaSourceOutputName": "dia_source_dayobs",
         "visitSummaryInputName": "visit_summary",
-        "visitInfoOutputName": "visit_summary_dayobs_14",
+        "visitInfoOutputName": "visit_summary_dayobs",
     },
     dimensions=("instrument", "day_obs"),
 ):
@@ -69,8 +69,9 @@ class ConsolidateSspTablesConnections(
         doc="Input Source Tables to be concatenated",
         name="{diaSourceInputName}",
         storageClass="ArrowAstropy",
-        dimensions=("instrument", "visit", "detector"),
+        dimensions=("instrument", "visit"),
         multiple=True,
+        minimum=0,
         deferLoad=True,
     )
     inputVisitSummaries = pipeBase.connectionTypes.PrerequisiteInput(
@@ -79,105 +80,76 @@ class ConsolidateSspTablesConnections(
         storageClass="ExposureCatalog",
         dimensions=("instrument", "visit"),
         multiple=True,
+        minimum=0,
         deferLoad=True,
     )
     outputDiaTable = pipeBase.connectionTypes.Output(
-        doc="Concatenated Source Table from all day_obs in the input. The day_obs"
-        "dimension would be the day_obs of the latest day_obs in the input.",
+        doc="Concatenated Source Table from one day_obs.",
         name="{diaSourceOutputName}",
         storageClass="DataFrame",
         dimensions=("instrument", "day_obs"),
     )
     outputVisitInfo = pipeBase.connectionTypes.Output(
-        doc="Concatenated Visit Summary from all day_obs in the input. The day_obs"
-        "dimension would be the day_obs of the latest day_obs in the input.",
+        doc="Concatenated Visit Summary from one day_obs.",
         name="{visitInfoOutputName}",
         storageClass="DataFrame",
         dimensions=("instrument", "day_obs"),
     )
 
-class ConsolidateSspTablesConfig(
-    pipeBase.PipelineTaskConfig, pipelineConnections=ConsolidateSspTablesConnections
+    def __init__(self, *, config=None):
+        super().__init__(config=config)
+
+        if not config.consolidateVisitTables:
+            self.prerequisiteInputs.remove("inputVisitSummaries")
+
+
+diaSourceColumnRenameDict = {'diaSourceId': 'idstring', 'visit': 'image', 'midpointMjdTai': 'MJD',
+                             'ra': 'RA', 'dec': 'Dec', 'trailLength': 'trail_len', 'trailAngle': 'trail_PA',
+                             'sourceId': 'idstring', 'expTime': 'exptime'}
+
+class ConsolidateSspDayobsConfig(
+    pipeBase.PipelineTaskConfig, pipelineConnections=ConsolidateSspDayobsConnections
 ):
     observatoryCode = lsst.pex.config.Field(
         dtype=str, default="X05", doc="Observatory code MPC, defaults to X05"
     )
+    consolidateVisitTables = lsst.pex.config.Field(
+        dtype=bool, default=True, doc="Whether to expect visit_summary-like data"
+    )
 
 
-class ConsolidateSspTablesTask(pipeBase.PipelineTask):
+class ConsolidateSspDayobsTask(pipeBase.PipelineTask):
     """Concatenate `sourceTable` list into a per-dayobs `sourceTable_dayobs`"""
 
-    ConfigClass = ConsolidateSspTablesConfig
-    _DefaultName = "consolidateSspTables"
+    ConfigClass = ConsolidateSspDayobsConfig
+    _DefaultName = "consolidateSspDayobs"
 
     def runQuantum(self, butlerQC, inputRefs, outputRefs):
         """Concatenate the input source tables into a single output table."""
         inputs = butlerQC.get(inputRefs)
         inputDiaTableRefs = inputs["inputDiaTables"]
-        inputVisitSummaryRefs = inputs["inputVisitSummaries"]
+        if len(inputDiaTableRefs) == 0:
+            raise pipeBase.NoWorkFound("No source tables found, skipping day_obs.")
 
         # Let's make the order deterministic by sorting the input Refs.
-        inputDiaTableRefs.sort(key=lambda x: (x.dataId["visit"], x.dataId["detector"]))
-
-        # Sort by visit only since no detector key in visitSummary dataId.
-        inputVisitSummaryRefs.sort(key=lambda x: x.dataId["visit"])
+        inputDiaTableRefs.sort(key=lambda x: (x.dataId["visit"]))
 
         # Visits in the visit summaries and unique visits in the DIA tables
         # should be the same.
         visitsInDia = set(ref.dataId["visit"] for ref in inputDiaTableRefs)
-        visitsInSummary = set(ref.dataId["visit"] for ref in inputVisitSummaryRefs)
-        # Some sanity checks.
-        assert len(visitsInSummary) == len(inputVisitSummaryRefs), "Duplicate visits in visit summaries."
-        assert visitsInDia == visitsInSummary, (
-            f"Mismatch in visits: {len(visitsInSummary)} visits in visit summaries, "
-            f"{len(visitsInDia)} unique visits in DIA tables."
-        )
 
         self.log.info(
-            f"Concatenating {len(inputVisitSummaryRefs)} visit summaries and {len(inputDiaTableRefs)} "
-            f"DIA source tables over {len(visitsInDia)} visits.",
+            f"Concatenating {len(inputDiaTableRefs)} DIA source tables over {len(visitsInDia)} visits.",
         )
 
         # Concatenate the input DIA tables into a single table without having
         # them all in memory at once.
-        consolidatedDiaTable = TableVStack.vstack_handles(inputDiaTableRefs)
+        consolidatedDiaTable = TableVStack.vstack_handles(inputDiaTableRefs).to_pandas()
+        consolidatedDiaTable = consolidatedDiaTable.rename(columns=diaSourceColumnRenameDict)
+        consolidatedDiaTable = consolidatedDiaTable[['MJD', 'RA', 'Dec', 'idstring']]
+        consolidatedDiaTable['idstring'] = consolidatedDiaTable['idstring'].astype(str)
+        consolidatedDiaTable['obscode'] = self.config.observatoryCode
 
-        # A list of dictionaries for each visit.
-        ccdEntries = []
-
-        for visitSummaryRef in inputVisitSummaryRefs:
-            # visitInfo is the same for all elements in visitSummary.
-            visitInfo = visitSummaryRef.get()[0].getVisitInfo()
-
-            # Populate an entry with the visitInfo data.
-            entry = dict(
-                visit=visitInfo.id,  # == 'visit' in consolidatedDiaTable.
-                exposureTime=visitInfo.exposureTime,
-                MJD=visitInfo.date.get(system=DateTime.MJD),
-                boresightRa=visitInfo.boresightRaDec[0].asDegrees(),
-                boresightDec=visitInfo.boresightRaDec[1].asDegrees(),
-                observatory=str(visitInfo.observatory),
-                instrumentLabel=visitInfo.instrumentLabel,
-                observationType=visitInfo.observationType,
-                scienceProgram=visitInfo.scienceProgram,
-                observationReason=visitInfo.observationReason,
-                object=visitInfo.object,
-                hasSimulatedContent=visitInfo.hasSimulatedContent,
-            )
-            ccdEntries.append(entry)
-
-            # We ideally want the observatory code e.g. X05 for LSST, from
-            # visitInfo, but since it's not available there, we retrieve it
-            # from config instead (see below).
-
-        # Make an Astropy table of visitInfo entries.
-        consolidatedVisitInfo = Table(rows=ccdEntries)
-        consolidatedVisitInfo["obsCode"] = self.config.observatoryCode
-        image = (
-            consolidatedVisitInfo[["MJD", "boresightRa", "boresightDec", "obsCode", "exposureTime"]]
-            .to_pandas()
-            .values
-        )
         obsCodesTextLines = ResourcePath("resource://heliolinx/obsCodes.txt").read().decode().split("\n")
         obsarr = solardg.parse_ObsCodes(obsCodesTextLines)
 
@@ -188,16 +160,75 @@ class ConsolidateSspTablesTask(pipeBase.PipelineTask):
             .rename(columns={"X": "x", "Y": "y", "Z": "z", "VX": "vx", "VY": "vy", "VZ": "vz"}),
             "EarthState",
         )
-        result = np.array(solardg.image_add_observerpos(image, obsarr, earthpos))
 
-        for col in ["X", "Y", "Z", "VX", "VY", "VZ"]:
-            consolidatedVisitInfo["observer" + col] = result[col]
+        if self.config.consolidateVisitTables:
+            inputVisitSummaryRefs = inputs["inputVisitSummaries"]
+            inputVisitSummaryRefs.sort(key=lambda x: x.dataId["visit"])
+            visitsInSummary = set(ref.dataId["visit"] for ref in inputVisitSummaryRefs)
+            # Some sanity checks.
+            assert len(visitsInSummary) == len(inputVisitSummaryRefs), "Duplicate visits in visit summaries."
+            assert visitsInDia == visitsInSummary, (
+                f"Mismatch in visits: {len(visitsInSummary)} visits in visit summaries, "
+                f"{len(visitsInDia)} unique visits in DIA tables."
+            )
 
+            # A list of dictionaries for each visit.
+            ccdEntries = []
+
+            for visitSummaryRef in inputVisitSummaryRefs:
+                # visitInfo is the same for all elements in visitSummary.
+                visitInfo = visitSummaryRef.get()[0].getVisitInfo()
+
+                # Populate an entry with the visitInfo data.
+                entry = dict(
+                    visit=visitInfo.id,  # == 'visit' in consolidatedDiaTable.
+                    exposureTime=visitInfo.exposureTime,
+                    MJD=visitInfo.date.get(system=DateTime.MJD),
+                    boresightRa=visitInfo.boresightRaDec[0].asDegrees(),
+                    boresightDec=visitInfo.boresightRaDec[1].asDegrees(),
+                    observatory=str(visitInfo.observatory),
+                    instrumentLabel=visitInfo.instrumentLabel,
+                    observationType=visitInfo.observationType,
+                    scienceProgram=visitInfo.scienceProgram,
+                    observationReason=visitInfo.observationReason,
+                    object=visitInfo.object,
+                    hasSimulatedContent=visitInfo.hasSimulatedContent,
+                )
+                ccdEntries.append(entry)
+
+                # We ideally want the observatory code e.g. X05 for LSST, from
+                # visitInfo, but since it's not available there, we retrieve it
+                # from config instead (see below).
+
+            # Make an Astropy table of visitInfo entries.
+            consolidatedVisitInfo = Table(rows=ccdEntries)
+            consolidatedVisitInfo["obsCode"] = self.config.observatoryCode
+            image = (
+                consolidatedVisitInfo[["MJD", "boresightRa", "boresightDec", "obsCode", "exposureTime"]]
+                .to_pandas()
+                .values
+            )
+            newimage = np.array(solardg.image_add_observerpos(image, obsarr, earthpos))
+
+        else:
+            hldet = utils.make_hldet(consolidatedDiaTable)
+            image = solardg.make_image_table(hldet)
+            mjd, ra, dec, obscode = image.T
+            mjd = mjd.astype(str).astype(float)
+            ra = ra.astype(str).astype(float)
+            dec = dec.astype(str).astype(float)
+            obscode = obscode.astype(str)
+            expTime = np.ones_like(ra) * 30.0  # TODO: Make exact.
+            image = np.array([mjd, ra, dec, obscode, expTime]).T
+            newimage = solardg.image_add_observerpos(image, obsarr, earthpos)
+
+        consolidatedVisitInfo = Table(newimage, names=['MJD', 'RA', 'Dec', 'obscode', 'X', 'Y', 'Z', 
+                                                       'VX', 'VY', 'VZ', 'startind', 'endind', 'exptime'])
         # Assign units to relevant columns.
-        consolidatedVisitInfo["exposureTime"].unit = u.second
+        consolidatedVisitInfo["exptime"].unit = u.second
         consolidatedVisitInfo["MJD"].unit = u.day
-        consolidatedVisitInfo["boresightRa"].unit = u.deg
-        consolidatedVisitInfo["boresightDec"].unit = u.deg
+        consolidatedVisitInfo["RA"].unit = u.deg
+        consolidatedVisitInfo["Dec"].unit = u.deg
 
         butlerQC.put(
             pipeBase.Struct(outputDiaTable=consolidatedDiaTable, outputVisitInfo=consolidatedVisitInfo),
